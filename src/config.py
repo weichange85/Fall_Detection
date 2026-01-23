c53589ec2b869d7ba5e0a99a1c21eeb531c2aa31
Wei Chang <wei.chang85@gmail.com>
2026-01-23 11:42:55 +1100

    test out inference

--- src/config.py
+++ src/config.py
@@ -7,5 +7,5 @@ BATCH_SIZE = 16
 
 
 #For inference and evaluation
-MODEL_PATH = "" # Path to the trained model weights
-INFERENCE_IMG_PATH = ""  # example image
+MODEL_PATH = "C:/Projects/Fall_Detection/notebooks/runs/detect/yolov8_train/weights/best.pt" # Path to the trained model weights
+INFERENCE_IMG_PATH = "C:/Projects/Fall_Detection/data/test/images/people-13-_jpg.rf.92e54676959f9b1f6e543f38423c82d5.jpg"  # example image
--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 from config import MODEL_PATH, INFERENCE_IMG_PATH
+import matplotlib.pyplot as plt
 import cv2
 
 def main():
@@ -7,8 +8,45 @@ def main():
 
     results = model.predict(INFERENCE_IMG_PATH)
 
-    # Show result
-    results.show()
+    boxes = results[0].boxes.xyxy.cpu().numpy()
+    confs = results[0].boxes.conf.cpu().numpy()
+    classes = results[0].boxes.cls.cpu().numpy()
+
+    ori_img = results[0].orig_img   # BGR image (numpy array)
+
+    # ---- Draw bounding boxes ----
+    for (x1, y1, x2, y2), conf, cls in zip(boxes, confs, classes):
+        x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
+
+        label = f"Fall ({conf:.2f})"
+
+        cv2.rectangle(
+            ori_img,
+            (x1, y1),
+            (x2, y2),
+            (0, 255, 0),
+            1
+        )
+
+        cv2.putText(
+            ori_img,
+            label,
+            (x1, max(0, y1 - 10)),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            1
+        )
+
+    # ---- Convert BGR -> RGB for matplotlib ----
+    ori_img_rgb = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
+
+    # ---- Display ----
+    plt.figure(figsize=(6, 6))
+    plt.imshow(ori_img_rgb)
+    plt.axis("off")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
