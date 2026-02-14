f95f9f93b1d069d87f4a1f87764019a0b0dc1911
Asif Alam <alamtheboss232@gmail.com>
2026-02-14 10:27:21 +0000

    update config.py with absolute paths for data and model files

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 
-DATA_YAML = r"C:\Projects\Fall_Detection\data\data.yaml"
+# DATA_YAML = r"C:\Projects\Fall_Detection\data\data.yaml"
+DATA_YAML = r"/Users/bdalam/Desktop/ml_project_wei_asif/Fall_Detection/data/data.yaml"
 MODEL_NAME = "yolov8n.pt"  # pretrained
 EPOCHS = 5
 IMGSZ = 640
@@ -7,5 +8,7 @@ BATCH_SIZE = 16
 
 
 #For inference and evaluation
-MODEL_PATH = "C:/Projects/Fall_Detection/notebooks/runs/detect/yolov8_train/weights/best.pt" # Path to the trained model weights
-INFERENCE_IMG_PATH = "C:/Projects/Fall_Detection/data/test/images/people-13-_jpg.rf.92e54676959f9b1f6e543f38423c82d5.jpg"  # example image
+# MODEL_PATH = "C:/Projects/Fall_Detection/notebooks/runs/detect/yolov8_train/weights/best.pt" # Path to the trained model weights
+MODEL_PATH = "/Users/bdalam/Desktop/ml_project_wei_asif/Fall_Detection/notebooks/runs/detect/yolov8_train/weights/best.pt"
+# INFERENCE_IMG_PATH = "C:/Projects/Fall_Detection/data/test/images/people-13-_jpg.rf.92e54676959f9b1f6e543f38423c82d5.jpg"  # example image
+INFERENCE_IMG_PATH = "/Users/bdalam/Desktop/ml_project_wei_asif/Fall_Detection/data/test/images/people-13-_jpg.rf.92e54676959f9b1f6e543f38423c82d5.jpg"  # example image
